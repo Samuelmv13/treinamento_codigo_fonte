d4cb8fdd52158c7bcecc7d5c9bd09e98d4f5fe54
Samuelmv13 <samuelmedeiros.v@outlook.com>
2021-07-24 20:29:24 -0300

    Regras de negócio e comunicação com o front implementado de responsável.

--- xtarefas/src/main/java/com/basis/campina/xtarefas/service/ResponsavelService.java
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/service/ResponsavelService.java
@@ -1,11 +1,41 @@
 package com.basis.campina.xtarefas.service;
 
+import com.basis.campina.xtarefas.domain.Responsavel;
+import com.basis.campina.xtarefas.repository.ResponsavelRepository;
+import com.basis.campina.xtarefas.service.dto.ResponsavelDTO;
+import com.basis.campina.xtarefas.service.mapper.ResponsavelMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ResponsavelService {
+
+    private final ResponsavelRepository repository;
+    private final ResponsavelMapper responsavelMapper;
+
+    public List<ResponsavelDTO> listarTodos() {
+        return repository.findAll().stream().map(responsavelMapper::toDto).collect(Collectors.toList());
+    }
+
+    public ResponsavelDTO buscarPorId(Integer id) {
+        Responsavel responsavel = repository.findById(id).orElseThrow(()->new RuntimeException("Responsável não encontrado"));
+        return responsavelMapper.toDto(responsavel);
+    }
+
+    public ResponsavelDTO salvar(ResponsavelDTO responsavelDTO) {
+        Responsavel objeto = responsavelMapper.toEntity(responsavelDTO);
+        objeto = repository.save(objeto);
+        return responsavelMapper.toDto(objeto);
+    }
+
+    public void remover(Integer id) {
+        buscarPorId(id);
+        repository.deleteById(id);
+    }
 }
--- xtarefas/src/main/java/com/basis/campina/xtarefas/web/rest/ResponsavelResource.java
+++ xtarefas/src/main/java/com/basis/campina/xtarefas/web/rest/ResponsavelResource.java
@@ -1,13 +1,58 @@
 package com.basis.campina.xtarefas.web.rest;
 
+import com.basis.campina.xtarefas.service.ResponsavelService;
+import com.basis.campina.xtarefas.service.dto.ResponsavelDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/responsaveis")
 @RequiredArgsConstructor
 @Slf4j
 public class ResponsavelResource {
+
+    private final ResponsavelService responsavelService;
+
+    @GetMapping
+    public ResponseEntity<List<ResponsavelDTO>> listarTodos() {
+        return ResponseEntity.ok(responsavelService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponsavelDTO> buscarPorId(@PathVariable Integer id) {
+        return ResponseEntity.ok(responsavelService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponsavelDTO> inserir(@RequestBody ResponsavelDTO responsavelDTO) throws URISyntaxException {
+        ResponsavelDTO dto = responsavelService.salvar(responsavelDTO);
+        return ResponseEntity.created(new URI("/api/responsaveis")).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponsavelDTO> atualizar(@PathVariable Integer id, @RequestBody ResponsavelDTO responsavelDTO) {
+        responsavelDTO.setId(id);
+        ResponsavelDTO dto = responsavelService.salvar(responsavelDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponsavelDTO> deletar(@PathVariable Integer id) {
+
+        responsavelService.remover(id);
+        return ResponseEntity.ok().build();
+    }
 }
